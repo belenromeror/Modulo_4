0e95cab57d762aeb6500448f6bceb5e38a69d800
Tu nombre de usuario en GitHub <Tu direccin de correo electrnico en GitHub>
2023-06-05 12:08:00 -0400

    Actividad

--- Sesión 9/comprobación.py
+++ Sesión 9/comprobación.py
@@ -6,7 +6,7 @@ def agregar_info_al_archivo(texto):
         print("ERROR: ARCHIVO NO ENCONTRADO")
     except Exception as e:
         print("ERROR: ", e)
-agregar_info_al_archivo("Hola Mundo")
+agregar_info_al_archivo("\nHola Mundo")
 agregar_info_al_archivo("Este en una nueva línea en el archivo")
 agregar_info_al_archivo("agregando la segunda línea del archivo")
 agregar_info_al_archivo("finalizando la línea agregada")
