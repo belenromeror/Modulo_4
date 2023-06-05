2057ed4cb83b385e1e46236551861cec5d03eb0c
Tu nombre de usuario en GitHub <Tu direccin de correo electrnico en GitHub>
2023-06-05 12:14:37 -0400

    Actividad

--- Sesión 9/comprobación.py
+++ Sesión 9/comprobación.py
@@ -12,32 +12,6 @@ agregar_info_al_archivo("agregando la segunda línea del archivo")
 agregar_info_al_archivo("finalizando la línea agregada")
 
 
-def reemplazar_datos_archivos(texto_viejo, texto_nuevo):
-    numero= 0
-    try:
-        archivo = open("informacion.dat", "r", encoding="utf-8")
-        lineas = archivo.readlines()
-        archivo.close()
-        reemplazo=""
-        for linea in lineas:
-            if texto_viejo in linea:
-                linea = linea.replace(texto_viejo, texto_nuevo)
-                reemplazo +=linea
-                numero +=1
-        if numero>0:
-            archivo_reemplazo =open("informacion.dat", "w", encoding ="utf-8")
-            archivo_reemplazo.write(reemplazo)   
-            archivo_reemplazo.close()
-    except FileNotFoundError:
-        print('No se encuentra el archivo datos.cvs')
-    except Exception as error:
-        print('Se ha generado un error no previsto',
-        type(error).__name__)
-    finally:
-        print("se realizaron", numero, "reemplazos")
-reemplazar_datos_archivos("información", "Procesamiento")
-
-
 
 
 
